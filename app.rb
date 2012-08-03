e4d4481a50b0b7662a11def5586fb40cce3a0e2e
phinze <paul.t.hinze@gmail.com>
2012-08-03 15:23:42 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+require 'braintree'
+
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.merchant_id = 'QuickStart_Merchant'
+Braintree::Configuration.public_key = 'QuickStart_Public'
+Braintree::Configuration.private_key = 'QuickStart_Private'
+
+get '/' do
+  erb :braintree
+end
+
+post '/' do
+  result = Braintree::Transaction.sale(:amount => "1000.00", :credit_card => {:number => params[:number], :cvv => params[:cvv], :expiration_month => params[:month], :expiration_year => params[:year]})
+
+  erb :braintree_response, :locals => {:result => result}
+end
